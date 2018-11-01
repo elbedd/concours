fbdce2843d2a870045b009d4cd72b88884bbd681
elbedd <laurentbraud.dev@gmail.coml>
2018-11-01 14:06:57 +0100

    Generation (main) De 10 à 100 equipes +  MAJ licence

--- src/main/java/com/elbedd/palet/Main.java
+++ src/main/java/com/elbedd/palet/Main.java
@@ -1,5 +1,5 @@
 // The MIT License (MIT)
-// Copyright (c) 2016 Laurent BRAUD
+// Copyright (c) 2016-2018 Laurent BRAUD
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
@@ -16,8 +16,8 @@ public class Main {
 	public static void main(String[] arg) {
 		int nbPartieQualificative = 5;
 
-		int nbEquipeMin = 8;
-		int nbEquipeMax = 15;
+		int nbEquipeMin = 10;
+		int nbEquipeMax = 100;
 		
 		for (int nbEquipe = nbEquipeMin; nbEquipe <= nbEquipeMax; nbEquipe++) {
 			Concours concours = new Concours(nbPartieQualificative);
